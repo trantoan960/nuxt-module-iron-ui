2bde2556ceec54e1a659560165d0bf77f6931f25
Alex Grozav <alex@grozav.com>
2022-02-05 14:19:31 +0200

    fix: Updated nuxt module reference.

--- tests/app/nuxt.config.ts
+++ tests/app/nuxt.config.ts
@@ -3,6 +3,6 @@ import { defineNuxtConfig } from 'nuxt3';
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
     buildModules: [
-        '~/modules/inkline/module.mjs'
+        './modules/inkline/module.mjs'
     ]
 });
